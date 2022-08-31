da4a5ec2a0f0734e499bb1bf449dd3744270d47d
Miguel Mdz <angelmendoza1451@gmail.com>
2022-08-31 16:23:17 -0500

    Creacion de la clase en controller

--- src/api/cursos/cursos.controller.js
+++ src/api/cursos/cursos.controller.js
@@ -1,51 +1,40 @@
 const cursosDAO = require('../../DAO/cursosDAO');
 
-async function crearCurso(req, res) {
-    try {
-        let data = await cursosDAO.crearCurso(req.body);
-        res.status(200).json(data);
-    } catch (err) {
-        return res.status(500).json({ status: 500, message: err.message });
+class CursosController {
+    async crearCurso(req, res) {
+        try {
+            let data = await cursosDAO.crearCurso(req.body);
+            res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json({ status: 500, message: err.message });
+        }
+    }
+    async obtenerCursos(req, res) {
+        try {
+            const body = Object.keys(req.body).length === 0 ? undefined : req.body;
+            let data = await cursosDAO.obtenerCursos(body);
+            res.status(200).json(data)
+        } catch (err) {
+            return res.status(500).json({ status: 500, message: err.message });
+        }
+    }
+    async actualizarCurso(req, res) {
+        try {
+            let data = await cursosDAO.actualizarCurso(req.body);
+            res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json({ status: 500, message: err.message });
+        }
+    }
+
+    async eliminarCurso(req, res) {
+        try {
+            const body = Object.keys(req.body).length === 0 ? undefined : req.body;
+            let data = await cursosDAO.eliminarCurso(body);
+            res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json({ status: 500, message: err.message });
+        }
     }
 }
-async function obtenerCursos(req, res) {
-    try {
-
-        const body = Object.keys(req.body).length === 0 ? undefined : req.body;
-        let data = await cursosDAO.obtenerCursos(body);
-        res.status(200).json(data)
-
-    } catch (err) {
-        return res.status(500).json({ status: 500, message: err.message });
-    }
-}
-async function actualizarCurso(req, res) {
-    try {
-
-        let data = await cursosDAO.actualizarCurso(req.body);
-        res.status(200).json(data);
-
-    } catch (err) {
-        return res.status(500).json({ status: 500, message: err.message });
-    }
-}
-
-async function eliminarCurso(req, res) {
-    try {
-
-        const body = Object.keys(req.body).length === 0 ? undefined : req.body;
-        let data = await cursosDAO.eliminarCurso(body);
-        res.status(200).json(data);
-
-    } catch (err) {
-        return res.status(500).json({ status: 500, message: err.message });
-
-    }
-}
-
-module.exports = {
-    crearCurso,
-    obtenerCursos,
-    actualizarCurso,
-    eliminarCurso
-}
+module.exports = new CursosController();
