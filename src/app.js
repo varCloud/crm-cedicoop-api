16648a3adf6a4e2a96617ae7499a81f4c065608a
Miguel Mdz <angelmendoza1451@gmail.com>
2022-09-02 17:26:33 -0500

    Implementacion del servicio Ventas

--- src/app.js
+++ src/app.js
@@ -53,6 +53,7 @@ app.use("/tiposervicios", require("./api/tiposervicios/tiposervicios"));
 app.use("/instructores", require("./api/instructores/instructores"));
 app.use("/tipoSeguimiento", require("./api/tipoSeguimiento/tipoSeguimiento"));
 app.use("/cotizaciones", require("./api/cotizaciones/cotizaciones"));
+app.use("/ventas", require('./api/ventas/ventas'))
 
 
 const swaggerSpec = swaggerJSDoc({
